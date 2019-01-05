e0413a92fb5784482dd859229885eaf337f0dbc5
oalejel <alejel.omar@gmail.com>
2019-01-05 14:51:06 -0500

    Updates

---
+++ precache-manifest.becb5825cb421ecdde90e66642fc7af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd3d3457989eb4492602",
+    "url": "/static/css/main.8f5a20f9.chunk.css"
+  },
+  {
+    "revision": "fd3d3457989eb4492602",
+    "url": "/static/js/main.fd3d3457.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0f092aa5ed18b80f676ca5839bf6cd7a",
+    "url": "/index.html"
+  }
+];
